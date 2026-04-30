2b0b6aee38c3795d3829050210575b5dc02f7257
saanvim-debug <saanvim@vt.edu>
2026-04-30 00:13:09 -0400

    Add files via upload

---
+++ project3/cs2505project3/datalab-handout/decl.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#define TMin INT_MIN
+#define TMax INT_MAX
+
+#include "btest.h"
+#include "bits.h"
+
+test_rec test_set[] = {
+/* Copyright (C) 1991-2021 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+/* This header is separate from features.h so that the compiler can
+   include it implicitly at the start of every compilation.  It must
+   not itself include <features.h> or any other header that includes
+   <features.h> because the implicit include comes before any feature
+   test macros that may be defined in a source file before it first
+   explicitly includes a system header.  GCC knows the name of this
+   header in order to preinclude it.  */
+/* glibc's intent is to support the IEC 559 math functionality, real
+   and complex.  If the GCC (4.9 and later) predefined macros
+   specifying compiler intent are available, use them to determine
+   whether the overall intent is to support these features; otherwise,
+   presume an older compiler has intent to support these features and
+   define these macros by default.  */
+/* wchar_t uses Unicode 10.0.0.  Version 10.0 of the Unicode Standard is
+   synchronized with ISO/IEC 10646:2017, fifth edition, plus
+   the following additions from Amendment 1 to the fifth edition:
+   - 56 emoji characters
+   - 285 hentaigana
+   - 3 additional Zanabazar Square characters */
+//1
+ {"thirdBits", (funct_t) thirdBits, (funct_t) test_thirdBits, 0,
+    "! ~ & ^ | + << >>", 8, 5,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+//2
+ {"dividePower2", (funct_t) dividePower2, (funct_t) test_dividePower2, 2,
+    "! ~ & ^ | + << >>", 15, 10,
+  {{TMin, TMax},{0,30},{TMin,TMax}}},
+//3
+ {"isNonNegative", (funct_t) isNonNegative, (funct_t) test_isNonNegative, 1,
+    "! ~ & ^ | + << >>", 6, 10,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+//4
+ {"remainderPower2", (funct_t) remainderPower2, (funct_t) test_remainderPower2, 2,
+    "! ~ & ^ | + << >>", 20, 10,
+  {{TMin, TMax},{0,30},{TMin,TMax}}},
+//5
+ {"replaceByte", (funct_t) replaceByte, (funct_t) test_replaceByte, 3,
+    "! ~ & ^ | + << >>", 10, 15,
+  {{TMin, TMax},{0,3},{0,255}}},
+//6
+ {"howManyBits", (funct_t) howManyBits, (funct_t) test_howManyBits, 1, "! ~ & ^ | + << >>", 90, 8,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+  {"", NULL, NULL, 0, "", 0, 0,
+   {{0, 0},{0,0},{0,0}}}
+};
